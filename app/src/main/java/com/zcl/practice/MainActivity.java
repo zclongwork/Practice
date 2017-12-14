7e44a84a9d41b38e4c877df60a838bcf9cbdf7ca
zclongwork <zclongwork@163.com>
2017-12-14 14:32:46 +0800

    bilibili弹幕demo

--- app/src/main/java/com/zcl/practice/MainActivity.java
+++ app/src/main/java/com/zcl/practice/MainActivity.java
@@ -1,10 +1,10 @@
 package com.zcl.practice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.zcl.practice.animation.AnimationActivity;
+import com.zcl.practice.danmakuflame.DemoActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,9 +86,16 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Intent i = new Intent(this, AnimationActivity.class);
+            startActivity(i);
+            
         } else if (id == R.id.nav_gallery) {
-
+    
+            Intent i = new Intent(this, DemoActivity.class);
+            startActivity(i);
         } else if (id == R.id.nav_slideshow) {
+    
+        
 
         } else if (id == R.id.nav_manage) {
 
