6861fc10e1f91281505fec5d2d0c8a0e85facbb9
zhangchenglong <chenglong@6.cn>
2019-10-11 10:25:16 +0800

    headfirst设计模式

---
+++ JavaDemo/src/main/java/com/zcl/javademo/MyClass.java
@@ -0,0 +1,68 @@
+package com.zcl.javademo;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MyClass {
+    
+    private static LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+    
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(15);
+        for (int i = 0; i < 5; i++) {
+            service.submit(new Productor(queue));
+        }
+        for (int i = 0; i < 10; i++) {
+            service.submit(new Consumer(queue));
+        }
+    }
+    
+    
+    static class Productor implements Runnable {
+        
+        private BlockingQueue queue;
+        
+        public Productor(BlockingQueue queue) {
+            this.queue = queue;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Random random = new Random();
+                    int i = random.nextInt();
+                    System.out.println("生产者" + Thread.currentThread().getName() + "生产数据" + i);
+                    queue.put(i);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    static class Consumer implements Runnable {
+        private BlockingQueue queue;
+        
+        public Consumer(BlockingQueue queue) {
+            this.queue = queue;
+        }
+        
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Integer element = (Integer) queue.take();
+                    System.out.println("消费者" + Thread.currentThread().getName() + "正在消费数据" + element);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    
+}
